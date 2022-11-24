518c26c9379ea13899cab45fa218642bd91b58e8
Hervé Pagès <hpages.on.github@gmail.com>
2022-11-23 16:54:56 -0800

    Add Prisca Chidimma Maduka as a contributor + various minor edits

--- R/downloadGenomicSequencesFromNCBI.R
+++ R/downloadGenomicSequencesFromNCBI.R
@@ -1,4 +1,6 @@
-### The function helps to download NCBI genomic sequence of a particular organism
+### A utility function to download the compressed FASTA file that contains
+### the genomic sequences of a given NCBI assembly.
+
 downloadGenomicSequencesFromNCBI <- function(assembly_accession,
                                              assembly_name=NA,
                                              destdir=".", method, quiet=FALSE)
@@ -8,16 +10,17 @@ downloadGenomicSequencesFromNCBI <- function(assembly_accession,
     if (!dir.exists(destdir))
         stop(wmsg("'destdir' must be the path to an existing directory"))
     if (file.access(destdir, 2))
-        stop(wmsg("'destdir'  must be a path with write permission"))
+        stop(wmsg("you don't have write permission to 'destdir'"))
     if (!isTRUEorFALSE(quiet))
         stop(wmsg("'quiet' must be TRUE or FALSE"))
 
     ftp_dir <- find_NCBI_assembly_ftp_dir(assembly_accession, assembly_name)
-    ftp_dir_url <- ftp_dir[[1]]
-    file_prefix <- ftp_dir[[2]]
-    filename <- paste0(file_prefix, "_", "genomic.fna.gz")
-    full_url <- paste0(ftp_dir_url, "/", filename)
-    destfile <- file.path(destdir, filename)
-    download.file(full_url, destfile, method, quiet)
+    ftp_dir_url <- ftp_dir[[1L]]
+    file_prefix <- ftp_dir[[2L]]
+    file_name <- paste0(file_prefix, "_genomic.fna.gz")
+    file_url <- paste0(ftp_dir_url, "/", file_name)
+    destfile <- file.path(destdir, file_name)
+    download.file(file_url, destfile, method, quiet)
     invisible(destfile)
 }
+
--- R/downloadGenomicSequencesFromUCSC.R
+++ R/downloadGenomicSequencesFromUCSC.R
@@ -1,21 +1,27 @@
-### The function helps to download UCSC genomic sequence of a particular organism
-downloadGenomicSequencesFromUCSC <- function(genome, goldenPath.url=getOption("UCSC.goldenPath.url"), destdir=".", method, quiet=FALSE)
-{   
-    if (!isSingleString(genome)) 
-        stop(wmsg("'genome' must be a single string")) 
-    if (!isSingleString(goldenPath.url)) 
+### A utility function to download the 2bit file that contains the genomic
+### sequences of a given UCSC genome.
+
+downloadGenomicSequencesFromUCSC <- function(genome,
+            goldenPath.url=getOption("UCSC.goldenPath.url"),
+            destdir=".", method, quiet=FALSE)
+{
+    if (!isSingleString(genome))
+        stop(wmsg("'genome' must be a single string"))
+    if (!isSingleString(goldenPath.url))
         stop(wmsg("'goldenPath.url' must be a single string"))
-    if (!isSingleString(destdir)) 
+    if (!isSingleString(destdir))
         stop(wmsg("'destdir' must be a single string"))
     if (!dir.exists(destdir))
         stop(wmsg("'destdir' must be the path to an existing directory"))
     if (file.access(destdir, 2))
         stop(wmsg("you don't have write permission to 'destdir'"))
-    if (!isTRUEorFALSE(quiet)) 
+    if (!isTRUEorFALSE(quiet))
         stop(wmsg("'quiet' must be TRUE or FALSE"))
+
     file_name <- paste0(genome, ".2bit")
     file_url <- paste0(goldenPath.url, "/", genome, "/bigZips/", file_name)
-    destfile <- file.path(destdir, file_name)        
+    destfile <- file.path(destdir, file_name)
     download.file(file_url, destfile, method, quiet)
     invisible(destfile)
-}                             
+}
+
