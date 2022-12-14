b72b4ce7c8ee7e81e7b99884afceebed8a4e5e33
Kirabo Kakopo <52150109+kakopo@users.noreply.github.com>
2022-12-14 20:45:03 +0300

    Implement, export, and document fastaTo2bit() (#6)

---
+++ R/fastaTo2bit.R
@@ -0,0 +1,16 @@
+### The function helps to convert a file from FASTA to 2bit
+fastaTo2bit <- function(origfile, destfile, assembly_accession = "")
+  
+{
+    if (!isSingleString(origfile))
+        stop(wmsg("'origfile' must be a single string"))
+    if (dir.exists(origfile))
+        stop(wmsg("'origfile' must be path to a file, not a directory"))
+    if (!file.exists(origfile))
+        stop(wmsg("'origfile' must be the path to an existing file"))
+    if (!isSingleString(destfile))
+        stop(wmsg("'destfile' must be a single string"))
+    dna <- readDNAStringSet(origfile)
+    export.2bit(dna, destfile)
+}
+
