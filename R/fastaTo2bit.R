c375d1f42f59ad1e87df09e99d159f43fe1d39aa
Kirabo Kakopo <kiraboismyname@gmail.com>
2022-12-20 03:39:06 +0300

    differentiate between GenBank and RefSeq assembly accession

--- R/fastaTo2bit.R
+++ R/fastaTo2bit.R
@@ -1,13 +1,28 @@
 ### The function helps to convert a file from FASTA to 2bit
 .sort_and_rename <- function(dna, assembly_accession)
 {
-    current_RefSeqAccn <- unlist(heads(strsplit(names(dna), " ", fixed=TRUE), n=1L))
+    current_Accn <- unlist(heads(strsplit(names(dna), " ", fixed=TRUE), n=1L))
     chrominfo <- getChromInfoFromNCBI(assembly_accession)
+
+    ### Check if RefSeq assembly accession
+    if(grepl("GCF", assembly_accession))
+{
     expected_RefSeqAccn <- chrominfo[ , "RefSeqAccn"]
-    stopifnot(setequal(expected_RefSeqAccn, current_RefSeqAccn))
+    stopifnot(setequal(expected_RefSeqAccn, current_Accn))
 
     ### Reorder the sequences.
-    dna <- dna[match(expected_RefSeqAccn, current_RefSeqAccn)]
+    dna <- dna[match(expected_RefSeqAccn, current_Accn)]
+}
+
+    ### Check if GenBank assembly accession
+    else if (grepl("GCA", assembly_accession))
+{
+    expected_GenBankAccn <- chrominfo[ , "GenBankAccn"]
+    stopifnot(setequal(expected_GenBankAccn, current_Accn))
+
+    ### Reorder the sequences.
+    dna <- dna[match(expected_GenBankAccn, current_Accn)]
+}
 
     ### Rename the sequences.
     names(dna) <- chrominfo[ , "SequenceName"]
@@ -15,6 +30,7 @@
     ### Check sequence lengths.
     expected_seqlengths <- chrominfo[ , "SequenceLength"]
     stopifnot(all(width(dna) == expected_seqlengths | is.na(expected_seqlengths)))
+    dna
 }
 
 fastaTo2bit <- function(origfile, destfile, assembly_accession=NA)
