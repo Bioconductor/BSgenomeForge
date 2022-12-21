4d1c6e6cebd5241507caf12959ca9ac4078aa276
Kirabo Kakopo <52150109+kakopo@users.noreply.github.com>
2022-12-21 18:22:52 +0300

    Add 'assembly_accession' argument to fastaTo2bit() (#8)

--- R/fastaTo2bit.R
+++ R/fastaTo2bit.R
@@ -1,6 +1,30 @@
 ### The function helps to convert a file from FASTA to 2bit
-fastaTo2bit <- function(origfile, destfile)
-  
+.sort_and_rename <- function(dna, assembly_accession)
+{
+    current_Accn <- unlist(heads(strsplit(names(dna), " ", fixed=TRUE), n=1L))
+    chrominfo <- getChromInfoFromNCBI(assembly_accession)
+
+    ### Check if RefSeq or GenBank assembly accession
+    if (grepl("GCF", assembly_accession)) {
+        expected_Accn <- chrominfo[ , "RefSeqAccn"]
+    } else if (grepl("GCA", assembly_accession)) {
+        expected_Accn <- chrominfo[ , "GenBankAccn"]
+    }
+    stopifnot(setequal(expected_Accn, current_Accn))
+
+    ### Reorder the sequences.
+    dna <- dna[match(expected_Accn, current_Accn)]
+
+    ### Rename the sequences.
+    names(dna) <- chrominfo[ , "SequenceName"]
+
+    ### Check sequence lengths.
+    expected_seqlengths <- chrominfo[ , "SequenceLength"]
+    stopifnot(all(width(dna) == expected_seqlengths | is.na(expected_seqlengths)))
+    dna
+}
+
+fastaTo2bit <- function(origfile, destfile, assembly_accession=NA)
 {
     if (!isSingleString(origfile))
         stop(wmsg("'origfile' must be a single string"))
@@ -10,7 +34,13 @@ fastaTo2bit <- function(origfile, destfile)
         stop(wmsg("'origfile' must be the path to an existing file"))
     if (!isSingleString(destfile))
         stop(wmsg("'destfile' must be a single string"))
-    dna <- readDNAStringSet(origfile)
-    export.2bit(dna, destfile)
-}
+    if (!isSingleStringOrNA(assembly_accession))
+        stop(wmsg("'assembly_accession' must be a single string or NA value"))
 
+    dna <- readDNAStringSet(origfile)
+
+    if (!is.na(assembly_accession))
+        dna <- .sort_and_rename(dna, assembly_accession)
+    ### Export file as 2bit
+    export.2bit (dna, destfile)
+}
