53a0ca57d53d12d81cb8ac715accec362adae8e6
Kirabo Kakopo <52150109+kakopo@users.noreply.github.com>
2022-12-22 22:44:07 +0300

    Add "NCBI_BSgenome_datapkg" package template to BSgenomeForge (#10)

---
+++ inst/pkgtemplates/NCBI_BSgenome_datapkg/R/zzz.R
@@ -0,0 +1,45 @@
+###
+###
+
+.pkgname <- "@PKGNAME@"
+
+.seqnames <- @SEQNAMES@
+
+.circ_seqs <- @CIRCSEQS@
+
+.mseqnames <- @MSEQNAMES@
+
+.onLoad <- function(libname, pkgname)
+{
+    if (pkgname != .pkgname)
+        stop("package name (", pkgname, ") is not ",
+             "the expected name (", .pkgname, ")")
+    extdata_dirpath <- system.file("extdata", package=pkgname,
+                                   lib.loc=libname, mustWork=TRUE)
+
+    ## Make and export BSgenome object.
+    bsgenome <- BSgenome(
+        organism="@ORGANISM@",
+        common_name=NA,
+        genome="@GENOME@",
+        provider="@PROVIDER@",
+        release_date=NA,
+        source_url=NA,
+        seqnames=.seqnames,
+        circ_seqs=.circ_seqs,
+        mseqnames=.mseqnames,
+        seqs_pkgname=pkgname,
+        seqs_dirpath=extdata_dirpath
+    )
+
+    ns <- asNamespace(pkgname)
+
+    objname <- pkgname
+    assign(objname, bsgenome, envir=ns)
+    namespaceExport(ns, objname)
+
+    old_objname <- "@BSGENOMEOBJNAME@"
+    assign(old_objname, bsgenome, envir=ns)
+    namespaceExport(ns, old_objname)
+}
+
