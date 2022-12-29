94f8ddccfbe2d1ad9fb5f59d367d330c94e16c2a
Kirabo Kakopo <kiraboismyname@gmail.com>
2022-12-29 21:23:28 +0300

    Minor edits to script, plus new man page

--- R/forgeBSgenomeDataPkgFromNCBI.R
+++ R/forgeBSgenomeDataPkgFromNCBI.R
@@ -1,38 +1,41 @@
 forgeBSgenomeDataPkgFromNCBI <- function(assembly_accession, organism, genome,
                                          pkg_maintainer, pkg_author=NA,
-                                         pkg_version="0.01", destdir=".")
+                                         pkg_version="0.01",
+                                         license="Artistic-2.0",
+                                         destdir=".")
 {
     local_file <-downloadGenomicSequencesFromNCBI(assembly_accession)
     origfile <- local_file
     fastaTo2bit(origfile, "single_sequences.2bit", assembly_accession)
 
     library(Biobase)
-    abbr <- abbreviate(organism)
-    pkgname <- paste0("BSgenome.", abbr, ".NCBI.", genome)
+    f_name <- substring(organism,1,1)
+    split_organism <- strsplit(organism, " +")[[1]]
+    l_name <- tail(split_organism, 1)
+    abbr_name <- paste0(f_name, l_name)
+    pkgname <- paste0("BSgenome.", abbr_name, ".NCBI.", genome)
 
     if(is.na(pkg_author))
         pkg_author <- pkg_maintainer
 
     pkgtitle <- paste0("Full genome sequences for ", organism, " (NCBI version ", genome, ")")
-    pkgdesc <- paste0("Full genome sequences for", organism, "as provided by NCBI (",
-                      genome, ") and stored in Biostrings objects.")
+    pkgdesc <- paste0("Full genome sequences for ", organism, "as provided by NCBI (", genome, ") and stored in Biostrings objects.")
 
     symValues <- list(PKGNAME = pkgname,
-                      BSGENOMEOBJNAME = abbr,
+                      BSGENOMEOBJNAME = abbr_name,
                       PKGTITLE = pkgtitle,
                       AUTHOR = pkg_author,
                       PKGVERSION = pkg_version,
                       MAINTAINER = pkg_maintainer,
                       PKGDESCRIPTION = pkgdesc,
-                      LICENSE = "Artistic-2.0",
+                      LICENSE = license,
                       ORGANISM = organism,
                       GENOME = genome,
                       ORGANISMBIOCVIEW = genome,
                       SEQNAMES = "NA",
                       CIRCSEQS = "character(0)")
 
-    #originDir <- "~/Documents/GitHub/BSgenomeForge/inst/pkgtemplates/NCBI_BSgenome_datapkg"
-    originDir <- system.file("inst", "pkgtemplates", "NCBI_BSgenome_datapkg", package = "BSgenomeForge")
+    originDir <- system.file("pkgtemplates", "NCBI_BSgenome_datapkg", package = "BSgenomeForge")
     file_dir <- createPackage(pkgname, destdir, originDir, symValues, unlink=TRUE, quiet=FALSE)
 
     current_dir <- getwd()
