bd4d6dee43f09645af3b43dc5f01f0ddc9fd0ecc
Hervé Pagès <hpages.on.github@gmail.com>
2023-02-07 21:46:29 -0800

    Add package template 'UCSC_BSgenome_datapkg'

---
+++ inst/pkgtemplates/UCSC_BSgenome_datapkg/R/zzz.R
@@ -0,0 +1,43 @@
+###
+###
+
+.pkgname <- "@PKGNAME@"
+
+.seqnames <- @SEQNAMES@
+
+.circ_seqs <- @CIRCSEQS@
+
+.onLoad <- function(libname, pkgname)
+{
+    if (pkgname != .pkgname)
+        stop("package name (", pkgname, ") is not ",
+             "the expected name (", .pkgname, ")")
+    extdata_dirpath <- system.file("extdata", package=pkgname,
+                                   lib.loc=libname, mustWork=TRUE)
+
+    ## Make and export BSgenome object.
+    bsgenome <- BSgenome(
+        organism="@ORGANISM@",
+        common_name=NA,
+        genome="@GENOME@",
+        provider="UCSC",
+        release_date=NA,
+        source_url=NA,
+        seqnames=.seqnames,
+        circ_seqs=.circ_seqs,
+        mseqnames=NULL,
+        seqs_pkgname=pkgname,
+        seqs_dirpath=extdata_dirpath
+    )
+
+    ns <- asNamespace(pkgname)
+
+    objname <- pkgname
+    assign(objname, bsgenome, envir=ns)
+    namespaceExport(ns, objname)
+
+    old_objname <- "@BSGENOMEOBJNAME@"
+    assign(old_objname, bsgenome, envir=ns)
+    namespaceExport(ns, old_objname)
+}
+
