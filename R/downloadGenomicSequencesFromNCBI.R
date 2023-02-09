f788e9a34ccaa25719df291a1f0c469055227a6e
Hervé Pagès <hpages.on.github@gmail.com>
2023-02-09 09:37:14 -0800

    Add internal helper get_URL_to_genomic_sequences_from_NCBI()

--- R/downloadGenomicSequencesFromNCBI.R
+++ R/downloadGenomicSequencesFromNCBI.R
@@ -6,6 +6,17 @@
 ### the genomic sequences of a given NCBI assembly.
 ###
 
+### NOT exported.
+get_URL_to_genomic_sequences_from_NCBI <- function(assembly_accession,
+                                                   assembly_name=NA)
+{
+    ftp_dir <- find_NCBI_assembly_ftp_dir(assembly_accession, assembly_name)
+    ftp_dir_url <- ftp_dir[[1L]]
+    file_prefix <- ftp_dir[[2L]]
+    file_name <- paste0(file_prefix, "_genomic.fna.gz")
+    paste0(ftp_dir_url, "/", file_name)
+}
+
 downloadGenomicSequencesFromNCBI <- function(assembly_accession,
                                              assembly_name=NA,
                                              destdir=".", method, quiet=FALSE)
@@ -19,12 +30,9 @@ downloadGenomicSequencesFromNCBI <- function(assembly_accession,
     if (!isTRUEorFALSE(quiet))
         stop(wmsg("'quiet' must be TRUE or FALSE"))
 
-    ftp_dir <- find_NCBI_assembly_ftp_dir(assembly_accession, assembly_name)
-    ftp_dir_url <- ftp_dir[[1L]]
-    file_prefix <- ftp_dir[[2L]]
-    file_name <- paste0(file_prefix, "_genomic.fna.gz")
-    file_url <- paste0(ftp_dir_url, "/", file_name)
-    destfile <- file.path(destdir, file_name)
+    file_url <- get_URL_to_genomic_sequences_from_NCBI(assembly_accession,
+                                                       assembly_name)
+    destfile <- file.path(destdir, basename(file_url))
     download.file(file_url, destfile, method, quiet)
     invisible(destfile)
 }
