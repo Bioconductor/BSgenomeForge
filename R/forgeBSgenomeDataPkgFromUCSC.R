ab39a969073fcbda64de70b8a1460363da0dbe24
Hervé Pagès <hpages.on.github@gmail.com>
2023-02-21 22:35:15 -0800

    Refactor .get_circ_seqs_from_NCBI() and .get_circ_seqs_from_UCSC() so that the 2 functions share as much code as possible.

--- R/forgeBSgenomeDataPkgFromUCSC.R
+++ R/forgeBSgenomeDataPkgFromUCSC.R
@@ -9,84 +9,23 @@
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ### .get_circ_seqs_from_UCSC()
 ###
-### TODO: Refactor .get_circ_seqs_from_UCSC() and .get_circ_seqs_from_NCBI()
-### so that they share as much code as possible.
-
-.check_circ_seqs <- function(circ_seqs)
-{
-    if (is.null(circ_seqs))
-        return(circ_seqs)
-    if (!is.character(circ_seqs))
-        stop(wmsg("'circ_seqs' must be NULL or a character vector"))
-    if (anyNA(circ_seqs))
-        stop(wmsg("'circ_seqs' cannot contain NA's"))
-    if ("" %in% circ_seqs)
-        stop(wmsg("'circ_seqs' cannot contain empty strings"))
-    if (anyDuplicated(circ_seqs))
-        stop(wmsg("'circ_seqs' cannot contain duplicate values"))
-    circ_seqs
-}
-
-.genome_has_no_assembled_molecules <- function(circ_seqs)
-{
-    if (length(circ_seqs) == 0)
-        return(character(0))
-    stop(wmsg("This UCSC genome contains no assembled molecules ",
-              "so cannot have circular sequences."))
-}
 
 ### 'circ_seqs' contains the circular sequences specified by the user.
 .get_circ_seqs_from_UCSC <- function(genome, chrominfo, circ_seqs=NULL)
 {
-    circ_seqs <- .check_circ_seqs(circ_seqs)
+    check_circ_seqs(circ_seqs)
     UCSC_genomes <- registered_UCSC_genomes()
+    seqnames <- chrominfo[ , "chrom"]
     if (genome %in% UCSC_genomes[ , "genome"]) {
         ## UCSC genome is registered.
-        known_circ_seqs <- chrominfo[chrominfo[ , "circular"], "SequenceName"]
-        if (is.null(circ_seqs))
-            return(known_circ_seqs)
-        if (setequal(circ_seqs, known_circ_seqs))
-            return(circ_seqs)
-        msg <- "This UCSC genome is registered in the GenomeInfoDb package "
-        if (length(known_circ_seqs) == 0) {
-            msg <- c(msg, "and it has no known circular sequences.")
-        } else {
-            in1string <- paste0("\"", known_circ_seqs, "\"", collapse=", ")
-            msg <- c(msg, "which means that its circular sequences are known ",
-                          "so you are not required to specify them. However, ",
-                          "if you do specify them, then they must match the ",
-                          "known ones. The circular sequences for registered ",
-                          "genome ", genome, " are: ", in1string)
-        }
-        stop(wmsg(msg))
+        FUN <- get_circ_seqs_for_registered_assembly_or_genome
+        is_xxx <- chrominfo[ , "circular"]
     } else {
         ## UCSC genome is **not** registered.
-        if (!("assembled-molecule" %in% chrominfo[ , "SequenceRole"]))
-            return(.genome_has_no_assembled_molecules(circ_seqs))
-        if (is.null(circ_seqs))
-            stop(wmsg("This UCSC genome is not registered in the ",
-                      "GenomeInfoDb package so I don't know what its ",
-                      "circular sequences are (if any). Please provide ",
-                      "their names in a character vector passed to ",
-                      "the 'circ_seqs' argument (set 'circ_seqs' to ",
-                      "character(0) if the genome has no circular ",
-                      "sequences)."))
-        ## The sequence names in 'circ_seqs' must belong to the genome.
-        if (anyNA(match(circ_seqs, chrominfo[ , "SequenceName"])))
-            stop(wmsg("'circ_seqs' contains sequence names that ",
-                      "do not belong to the specified genome (",
-                      genome, ")"))
-        ## 'is_assembled' will be a logical vector with 1 element per row
-        ## in the 'chrominfo' data frame.
-        is_assembled <- chrominfo[ , "SequenceRole"] %in% "assembled-molecule"
-        assembled_molecules <- chrominfo[is_assembled, "SequenceName"]
-        ## The sequence names in 'circ_seqs' must be names of assembled
-        ## molecules.
-        if (!all(circ_seqs %in% assembled_molecules))
-            stop(wmsg("all the sequence names in 'circ_seqs' must be ",
-                      "names of assembled molecules"))
-        return(circ_seqs)
+        FUN <- get_circ_seqs_for_unregistered_assembly_or_genome
+        is_xxx <- NULL
     }
+    FUN(genome, seqnames, is_xxx, circ_seqs, what="UCSC genome")
 }
 
 
